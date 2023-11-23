23c4ad89d2d6b3678b01af062c98f1c4ec3b6542
estradax <azharumuhammad@gmail.com>
2023-11-23 12:15:43 +0700

    move to class based

--- src/main.cpp
+++ src/main.cpp
@@ -4,52 +4,70 @@
 #include <algorithm>
 
 static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }).base(), s.end());
+  s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+    return !std::isspace(ch);
+  }).base(), s.end());
 }
 
-std::vector<std::string> ParseInput(std::string s) {
-  std::vector<std::string> args;
+class Shell {
+public:
+  static void MainLoop() {
+    Shell shell;
 
-  rtrim(s);
+    std::string input;
 
-  std::size_t pos = 0;
-  while ((pos = s.find(' ')) != std::string::npos) {
-      args.push_back(s.substr(0, pos));
-      s.erase(0, pos + 1);
-  }
-  args.push_back(s);
-
-  return args;
-}
-
-bool ParseArgs(const std::vector<std::string> &args) {
-  if (args.size() < 1) {
-    return true;
+    while (shell.IsRunning()) {
+      std::cout << ">> ";
+      std::getline(std::cin, input);
+      
+      auto args = shell.Tokenize(input);
+      shell.ParseArgs(args);
+    }
   }
 
-  auto cmd = args[0];
-  if (cmd == "exit") {
-    return false;
-  } 
+private:
+  Shell() : is_running_{true} {}
 
-  std::cout << "command not found: " << cmd << '\n';
-  return true;
-}
+  bool IsRunning() const {
+    return is_running_;
+  }
+
+  std::vector<std::string> Tokenize(std::string s) const {
+    std::vector<std::string> args;
+
+    rtrim(s);
+
+    std::size_t pos = 0;
+    while ((pos = s.find(' ')) != std::string::npos) {
+        args.push_back(s.substr(0, pos));
+        s.erase(0, pos + 1);
+    }
+    args.push_back(s);
+
+    return args;
+  }
+
+  void ParseArgs(const std::vector<std::string> &args) {
+    if (args.size() < 1) {
+      return;
+    }
+
+    auto cmd = args[0];
+    if (cmd == "exit") {
+      is_running_ = false;
+      return;
+    } 
+
+    std::cout << "command not found: " << cmd << '\n';
+    return;
+  }
+
+private:
+  bool is_running_;
+};
 
 int main() {
-  std::string input;
-
-  auto running = true;
-
-  while (running) {
-    std::cout << ">> ";
-    std::getline(std::cin, input);
-    
-    auto args = ParseInput(input);
-    running = ParseArgs(args);
-  }
+  Shell::MainLoop();
 
   return 0;
 }
